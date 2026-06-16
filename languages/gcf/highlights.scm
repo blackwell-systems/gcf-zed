95a5e150ff68b79ffcb2572454f28f61e47b482e
Dayna Blackwell <dayna@blackwell-systems.com>
2026-06-15 17:58:54 -0700

    feat: GCF syntax highlighting extension for Zed

---
+++ languages/gcf/highlights.scm
@@ -0,0 +1,62 @@
+; Header
+(gcf_keyword) @keyword
+(header_pair (header_key) @property)
+(header_pair (header_value) @number)
+
+; Section headers
+"##" @keyword
+(section_name) @type
+(count_bracket "[" @punctuation.bracket)
+(count_bracket "]" @punctuation.bracket)
+(count_number) @number
+(deferred_marker) @operator
+(field_decl "{" @punctuation.bracket)
+(field_decl "}" @punctuation.bracket)
+(field_name) @property
+
+; Symbol lines
+(local_id "@" @punctuation.special)
+(id_number) @number
+(symbol_line (kind) @type.builtin)
+(symbol_line (qualified_name) @function)
+(symbol_line (score) @number.float)
+(symbol_line (provenance) @attribute)
+
+; Edge lines
+"<" @operator
+(edge_type) @label
+(edge_status) @keyword
+
+; Bare references
+(ref_line) @comment
+
+; Key-value pairs
+(kv_line (kv_key) @property)
+(kv_line (kv_value) @string)
+"=" @operator
+
+; Summary
+"##!" @keyword
+"summary" @keyword
+(summary_line (kv_pair (kv_key) @property))
+(summary_line (kv_pair (kv_value) @number))
+
+; Root scalar
+(root_scalar (scalar_value) @string)
+
+; Attachment lines
+(attachment_line "." @punctuation.special)
+(attachment_name) @property
+(attachment_object) @punctuation.bracket
+
+; Expanded items
+(expanded_item (local_id) @punctuation.special)
+
+; Tabular rows
+(tabular_row) @string
+
+; Comments
+(comment) @comment
+
+; Text fallback
+(text_content) @string
